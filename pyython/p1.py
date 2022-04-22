4c7b1bc3e507d6cb8b1af36f4036590d40662ed5
Yashswini <83688904+Yashswini@users.noreply.github.com>
2022-04-22 21:53:44 +0530

    Add files via upload

---
+++ pyython/p1.py
@@ -0,0 +1,14 @@
+from ssl import match_hostname
+
+
+print("enter physics maarks")
+PhysicsMarks =int(input())
+
+print("enter Maths maarks")
+MathsMarks =int(input())
+
+if PhysicsMarks>MathsMarks:
+    print("is greater")
+else :
+    
+        print ("Is smaller")
